92777eed407189f59817763f03cbb4f7aadfa988
PechenkoVkysnoe <79215808+PechenkoVkysnoe@users.noreply.github.com>
2021-03-17 00:12:26 +0300

    Update test.c

--- clabs/test.c
+++ clabs/test.c
@@ -3,12 +3,12 @@
 
 void test(){
     assert(convert_rub(2.0)==2);
-    /*assert(convert_rub(2.99)==2);
+    assert(convert_rub(2.99)==2);
     assert(convert_rub(2.15645645245245)==2);
     assert(convert_pen(2.15,2)==15);
     assert(convert_pen(2.289,2)==29);
     assert(convert_pen(2.299,2)==30);
-    assert(convert_pen(2.0009,2)==0);*/
+    assert(convert_pen(2.0009,2)==0);
 }
 #undef main
 int main(){
