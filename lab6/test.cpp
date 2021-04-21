35b850fd67682be63bc5b720eff8eede55475c5f
PechenkoVkysnoe <1mishagrigorchuk1@mail.ru>
2021-04-21 13:26:56 +0300

    Update test.cpp

--- lab6/test.cpp
+++ lab6/test.cpp
@@ -7,6 +7,15 @@ void test() {
     assert(tree.MaxDepth() == 1);
     tree.insert('b');
     assert(tree.MaxDepth() == 2);
+    tree.insert('a');
+    tree.insert('c');
+    assert(tree.MaxDepth() == 3);
+    tree.insert('m');
+    tree.insert('i');
+    tree.insert('s');
+    tree.insert('h');
+    tree.insert('a');
+    assert(tree.MaxDepth() == 6);
 }
 
 int main() {
