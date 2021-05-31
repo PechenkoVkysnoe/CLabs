89c98a23f6f2e8e07106e3aa88f4f5ab058d5146
PechenkoVkysnoe <1mishagrigorchuk1@mail.ru>
2021-05-31 15:47:09 +0300

    lab7

---
+++ lab7/main_test.cpp
@@ -0,0 +1,22 @@
+// #include <assert.h>
+
+
+// void test() {
+//     DoubleList list1,list2,res,test;
+//     list1.push_back(1);
+//     list1.push_back(2);
+//     list2.push_back(3);
+//     list2.push_back(4);
+//     test.push_back(1);
+//     test.push_back(2);
+//     test.push_back(3);
+//     test.push_back(4);
+//     res = DoubleList::merge(list1, list2);
+//     assert(test==res);
+    
+// }
+
+// int main() {
+//     test();
+//     return 0;
+// }
